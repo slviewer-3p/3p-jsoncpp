7f117500cb2003b4623e645489c17d3e8559dddf
Nat Goodspeed <nat@lindenlab.com>
2016-03-24 17:56:55 -0400

    DRTVWR-418: Tweak build-cmd.sh for 64-bit builds and build-variables.

--- build-cmd.sh
+++ build-cmd.sh
@@ -6,22 +6,30 @@ cd "$(dirname "$0")"
 set -x
 # make errors fatal
 set -e
+# complain about unset env variables
+set -u
 
-JSONCPP_VERSION="0.5.0"
-JSONCPP_SOURCE_DIR="jsoncpp-src-"$JSONCPP_VERSION
+JSONCPP_SOURCE_DIR="jsoncpp-src"
+# version number is conveniently found in a file with no other content
+JSONCPP_VERSION="$(<$JSONCPP_SOURCE_DIR/version)"
 
 if [ -z "$AUTOBUILD" ] ; then 
     fail
 fi
 
 if [ "$OSTYPE" = "cygwin" ] ; then
-    export AUTOBUILD="$(cygpath -u $AUTOBUILD)"
+    autobuild="$(cygpath -u $AUTOBUILD)"
+else
+    autobuild="$AUTOBUILD"
 fi
 
 # load autobuild provided shell functions and variables
 set +x
-eval "$("$AUTOBUILD" source_environment)"
+eval "$("$autobuild" source_environment)"
 set -x
+
+# set LL_BUILD and friends
+set_build_variables convenience Release
 
 stage="$(pwd)/stage"
 
@@ -33,19 +41,21 @@ pushd "$JSONCPP_SOURCE_DIR"
         windows*)
             load_vsvars
 
-            build_sln "./makefiles/vs120/jsoncpp.sln" "Release|$AUTOBUILD_WIN_VSPLATFORM"
+            build_sln "./makefiles/vs$AUTOBUILD_VSVER/jsoncpp.sln" "Release|$AUTOBUILD_WIN_VSPLATFORM"
 
             mkdir --parents "$stage/lib/release"
             mkdir --parents "$stage/include/json"
 
             if [ "$AUTOBUILD_ADDRSIZE" = 32 ]
-            then cp ./build/vs120/release/lib_json/json_libmd.lib "$stage/lib/release"
-            else cp ./makefiles/vs120/x64/release/json_libmd.lib "$stage/lib/release"
+            then cp "./build/vs$AUTOBUILD_VSVER/release/lib_json/json_libmd.lib" "$stage/lib/release"
+            else cp "./makefiles/vs$AUTOBUILD_VSVER/x64/release/json_libmd.lib" "$stage/lib/release"
             fi
 
             cp ../"${JSONCPP_SOURCE_DIR}"/include/json/*.h "$stage/include/json"
         ;;
-        "darwin")
+        darwin*)
+            export CCFLAGS="-arch $AUTOBUILD_CONFIGURE_ARCH $LL_BUILD"
+            export CXXFLAGS="$CCFLAGS"
             ./scons.py platform=darwin
 
             mkdir -p "$stage/lib/release"
@@ -53,7 +63,9 @@ pushd "$JSONCPP_SOURCE_DIR"
             cp lib/release/*.a "$stage/lib/release"
             cp include/json/*.h "$stage/include/json"
         ;;
-        "linux")
+        linux*)
+            export CCFLAGS="-m$AUTOBUILD_ADDRSIZE $LL_BUILD"
+            export CXXFLAGS="$CCFLAGS"
             ./scons.py platform=linux-gcc
 
             mkdir -p "$stage/lib/release"
